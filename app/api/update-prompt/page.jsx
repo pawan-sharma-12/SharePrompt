058390e848a8533d167cb0343511d3e2d8686460
pawan-sharma-12 <ps2020nitsri@gmail.com>
2024-04-07 12:13:45 +0530

    created seperate module for EditPrompt.jsx and updated added suspend in /api/update-prompt

--- app/api/update-prompt/page.jsx
+++ app/api/update-prompt/page.jsx
@@ -1,65 +1,12 @@
-"use client";
-import { useRouter,useSearchParams } from "next/navigation"
-import { useEffect, useState } from "react"
-import Form from "@components/Form"
-const EditPrompt = () => {
-    const router = useRouter();
-    const [submitting, setSubmitting] = useState(false)
-    const searchParams = useSearchParams()
-    const promptId = searchParams.get('id')
+import EditPrompt from "@components/EditPrompt"
+import { Suspense } from "react"
 
-    const [post,setPost] = useState({
-        prompt : '',
-        tag : '',
-    })
-    useEffect(()=>{
-        const getPromptDetails = async () =>{
-           const response =  await fetch(`/api/prompt/${promptId}`)
-           const data = await response.json()
-           setPost({
-            prompt : data.prompt,
-            tag : data.tag
-           })
-        }
-        if(promptId){
-            getPromptDetails()
-        }
-
-    },[promptId])
-
-    const updatePrompt = async(e) =>{
-        e.preventDefault();
-        setSubmitting(true)
-        if(!promptId) return alert('Prompt ID not found ')
-        try {
-            const res = await fetch(`/api/prompt/${promptId}`,
-            {
-                method : 'PATCH',
-                body : JSON.stringify({
-                    prompt : post.prompt,
-                    tag : post.tag
-                })
-            }
-            )
-            if(res.ok){
-             
-                router.push('/')
-            }
-        } catch (error) {
-            console.log('failed to create the prompt ',error);
-            
-        } finally{
-            setSubmitting(false)
-        }
-    }
-   
+const UpdatePrompt = () => {
   return (
-    <Form 
-    type = "Edit" post = {post} setPost = {setPost} submitting = {submitting} 
-    handleSubmit = {updatePrompt}
-
-    />
+   <Suspense fallback = "Loading...">
+    <EditPrompt/>
+   </Suspense>
   )
 }
 
-export default EditPrompt
+export default UpdatePrompt
